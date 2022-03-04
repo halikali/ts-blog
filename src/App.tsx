5963ad8c214e42d3791e35273881dd14ed3a4d6f
halikali <tunchalikali@gmail.com>
2022-03-05 00:35:33 +0300

    map routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import HomePage from "pages/HomePage";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 
 import { exampleAction } from "store/actions/exampleAction";
+import routes from "routes";
 import "./App.scss";
 
 const App = () => {
@@ -12,12 +12,13 @@ const App = () => {
   useEffect(() => {
     dispatch(exampleAction());
   }, []);
+
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </div>
+    <Routes>
+      {routes.map((route) => (
+        <Route path={route.path} element={route.page} key={route.name} />
+      ))}
+    </Routes>
   );
 };
 
