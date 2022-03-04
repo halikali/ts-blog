ac4c95f74271a2e9502534f650d804e0fdf46820
halikali <tunchalikali@gmail.com>
2022-03-05 00:22:02 +0300

    create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import HomePage from "pages/HomePage";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+
 import { exampleAction } from "store/actions/exampleAction";
 import "./App.scss";
 
@@ -9,7 +12,13 @@ const App = () => {
   useEffect(() => {
     dispatch(exampleAction());
   }, []);
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
