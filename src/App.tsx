88a619c3c701c068b7916961af0d059573139f7e
halikali <tunchalikali@gmail.com>
2022-03-04 23:44:46 +0300

    dispatch example action

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { exampleAction } from "store/actions/exampleAction";
 import "./App.scss";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(exampleAction());
+  }, []);
   return <div></div>;
 };
 
