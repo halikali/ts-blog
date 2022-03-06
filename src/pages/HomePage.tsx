0bc9dbc7e92354006916fdb63c40e820fea32a70
halikali <tunchalikali@gmail.com>
2022-03-06 14:57:39 +0300

    import recent posts component in the app component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,18 @@
-import Header from "components/Header";
-import React from "react";
+import LandingCards from "components/Landing-cards";
+import RecentPosts from "components/Recent-posts";
 
 const HomePage = () => {
-  return <></>;
+  return (
+    <>
+      <LandingCards />
+      <div className="row">
+        <div className="col-12 col-lg-8">
+          <RecentPosts />
+        </div>
+        <div className="d-none d-lg-block col-12 col-md-4"></div>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
