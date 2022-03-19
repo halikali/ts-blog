c3615596267eb0d297258f1f046e71f0aec04700
halikali <tunchalikali@gmail.com>
2022-03-19 03:51:04 +0300

    complete the social media component

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -1,7 +1,41 @@
-import "./SocialMedia.css";
+import {
+  faFacebookF,
+  faGooglePlus,
+  faGooglePlusG,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./SocialMedia.scss";
 
 const SocialMedia = () => {
-  return <div>SocialMedia</div>;
+  return (
+    <div className="mt-5">
+      <p className="social-media__text-wrapper">
+        <span className="social-media__text">SOCIAL MEDIA</span>
+      </p>
+
+      <div className="social-media__wrapper">
+        <div className="social-media social-media--facebook">
+          <FontAwesomeIcon className="social-media__logo" icon={faFacebookF} />
+          <p>21.2K</p>
+          <p>Followers</p>
+        </div>
+        <div className="social-media social-media--twitter">
+          <FontAwesomeIcon className="social-media__logo" icon={faTwitter} />
+          <p>10.2K</p>
+          <p>Followers</p>
+        </div>
+        <div className="social-media social-media--google-plus">
+          <FontAwesomeIcon
+            className="social-media__logo"
+            icon={faGooglePlusG}
+          />
+          <p>5K</p>
+          <p>Followers</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SocialMedia;
