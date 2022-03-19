d469dafd9ad079ae244e37e19bad0f95b62518ce
halikali <tunchalikali@gmail.com>
2022-03-20 00:54:23 +0300

    complete multinews component

--- src/components/Multi-news/MultiNews.tsx
+++ src/components/Multi-news/MultiNews.tsx
@@ -1,7 +1,20 @@
+import SmallPost from "components/Small-Post";
 import "./MultiNews.scss";
 
 const MultiNews = () => {
-  return <div>MultiNews</div>;
+  return (
+    <div className="multi-news col-12 ">
+      <SmallPost />
+      <SmallPost />
+      <SmallPost />
+      <SmallPost />
+      <SmallPost />
+      <SmallPost />
+      <SmallPost />
+      <SmallPost />
+      <SmallPost />
+    </div>
+  );
 };
 
 export default MultiNews;
