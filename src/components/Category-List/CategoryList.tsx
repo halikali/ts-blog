589057f880ba9e43ef2d1454886795d7f870dc9d
halikali <tunchalikali@gmail.com>
2022-03-19 23:47:34 +0300

    move category list component to components

---
+++ src/components/Category-List/CategoryList.tsx
@@ -0,0 +1,30 @@
+import "./CategoryList.scss";
+
+const CategoryList = () => {
+  return (
+    <div className="category-list__wrapper">
+      <p className="category-list__name-wrapper">
+        <span className="category-list__name"> category list </span>
+      </p>
+      <ul className="category-list">
+        <li className="category-list__item">
+          lifestyle <p>451</p>
+        </li>
+        <li className="category-list__item">
+          fashion <p>451</p>
+        </li>
+        <li className="category-list__item">
+          technology <p>451</p>
+        </li>
+        <li className="category-list__item">
+          travel <p>451</p>
+        </li>
+        <li className="category-list__item">
+          health <p>451</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default CategoryList;
