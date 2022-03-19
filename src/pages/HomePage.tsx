124a238b0d9fd4aa625d585863891a6af12ca49c
halikali <tunchalikali@gmail.com>
2022-03-20 00:55:49 +0300

    import single category post component to home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,6 +5,9 @@ import LandingCards from "components/Landing-cards";
 import PopularPosts from "components/Popular-posts";
 import RecentPosts from "components/Recent-posts";
 import SocialMedia from "components/SocialMedia";
+import MultiNews from "components/Multi-news";
+import SingleCategoryPost from "components/Single-category-post";
+import Post from "components/Post";
 
 const HomePage = () => {
   return (
@@ -23,6 +26,26 @@ const HomePage = () => {
           <SocialMedia />
           <PopularPosts />
         </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-lg-4">
+          <SingleCategoryPost categoryName="lifestyle">
+            <Post type="type-1" />
+          </SingleCategoryPost>
+        </div>
+        <div className="col-12 col-lg-4">
+          <SingleCategoryPost categoryName="fashion">
+            <Post type="type-1" />
+          </SingleCategoryPost>
+        </div>
+        <div className="col-12 col-lg-4">
+          <SingleCategoryPost categoryName="health">
+            <Post type="type-1" />
+          </SingleCategoryPost>
+        </div>
+      </div>
+      <div className="row">
+        <MultiNews />
       </div>
     </>
   );
