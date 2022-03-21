a619ef11f85240ffd23de9b8e9eb6710c7427d65
halikali <tunchalikali@gmail.com>
2022-03-21 22:18:54 +0300

    complete footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,32 @@
 import "./Footer.scss";
 
 const Footer = () => {
-  return <footer className="container"></footer>;
+  return (
+    <footer className="container footer">
+      <div className="footer__body">
+        <p>Copyright &copy; {new Date().getFullYear()} All rights reserved</p>
+        <nav>
+          <ul className="footer__list-wrapper">
+            <li className="footer__list-item">
+              <a href="">Home</a>{" "}
+            </li>
+            <li className="footer__list-item">
+              <a href="">About Us</a>{" "}
+            </li>
+            <li className="footer__list-item">
+              <a href="">Contacts</a>{" "}
+            </li>
+            <li className="footer__list-item">
+              <a href="">Advertise</a>{" "}
+            </li>
+            <li className="footer__list-item">
+              <a href="">Privacy</a>{" "}
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
