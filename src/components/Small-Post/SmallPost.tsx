d9a6719b7db6667be80285dc6f5eaff85497b27c
halikali <tunchalikali@gmail.com>
2022-03-27 13:17:58 +0300

    fix styling of the small card component

--- src/components/Small-Post/SmallPost.tsx
+++ src/components/Small-Post/SmallPost.tsx
@@ -3,11 +3,13 @@ import "./SmallCard.scss";
 const SmallPost = () => {
   return (
     <div className="small-card">
-      <img
-        src="https://image.shutterstock.com/image-photo/underpass-sign-sky-background-600w-2137028029.jpg"
-        alt=""
-        className="small-card__image"
-      />
+      <div className="small-card__image-wrapper">
+        <img
+          src="https://image.shutterstock.com/image-photo/underpass-sign-sky-background-600w-2137028029.jpg"
+          alt=""
+          className="small-card__image"
+        />
+      </div>
       <div className="small-card__content">
         <p className="small-card__category-name">LIFESTYLE</p>
         <h3 className="small-card__title">
