5dbeedcefb8d8ba411c29dee2368ee56588ab6ff
halikali <tunchalikali@gmail.com>
2022-03-27 04:16:03 +0300

    add routes for details page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,3 +1,4 @@
+import DetailPage from "pages/DetailPage";
 import HomePage from "pages/HomePage";
 
 const routes = [
@@ -5,6 +6,11 @@ const routes = [
     path: "/",
     page: <HomePage />,
     name: "homePage",
+  },
+  {
+    path: "/details",
+    page: <DetailPage />,
+    name: "detailPage",
   },
 ];
 
