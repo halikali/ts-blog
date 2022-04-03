f74f09c6b22324497e1583197f9d29e2c81fc130
halikali <tunchalikali@gmail.com>
2022-04-03 13:43:16 +0300

    create the create post page

---
+++ src/pages/CreatePostPage.tsx
@@ -0,0 +1,11 @@
+import CreatePost from "components/Create-post";
+
+const CreatePostPage = () => {
+  return (
+    <div className="col-12 col-lg-8 m-auto">
+      <CreatePost />
+    </div>
+  );
+};
+
+export default CreatePostPage;
