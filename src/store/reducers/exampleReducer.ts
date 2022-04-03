e10eb2fd49a227b4cd395ac9bddfd40a709d0fc7
halikali <tunchalikali@gmail.com>
2022-04-03 13:46:29 +0300

    example reducer is serving for the post action

--- src/store/reducers/exampleReducer.ts
+++ src/store/reducers/exampleReducer.ts
@@ -1,11 +1,20 @@
+import { IPostAction } from "types/PostTypes";
+
 const initialState = {
   data: [],
+  isLoad: false,
+  error: null,
 };
 
-const exampleReducer = (state = initialState, action: any) => {
+const exampleReducer = (state = initialState, action: IPostAction) => {
   switch (action.type) {
-    case "EXAMPLE_ACTION":
-      return { ...state, data: action.payload };
+    case "CREATE_POST_REQUEST":
+      return { ...state, isLoad: false };
+
+    case "CREATE_POST_SUCCESS":
+      return { ...state, isLoad: true, data: [...state.data, action.payload] };
+    case "CREATE_POST_FAILURE":
+      return { ...state, isLoad: true, error: action.error };
     default:
       return state;
   }
