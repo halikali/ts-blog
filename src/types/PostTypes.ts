086372e1bb8ae56f51613b65202521bccf537a6f
halikali <tunchalikali@gmail.com>
2022-04-03 13:45:08 +0300

    create post types

---
+++ src/types/PostTypes.ts
@@ -0,0 +1,37 @@
+export interface IPost {
+  category: string;
+  title: string;
+  coverImageURL: string;
+  content: string;
+  contentImageURL: string;
+  tags: string[];
+  author: string;
+  createdAt: Date;
+}
+
+export interface IPostState {
+  posts: IPost[];
+}
+
+export interface IPostAction {
+  type: string;
+  payload?: any;
+  error?: any;
+}
+
+interface CREATE_POST_REQUEST {
+  type: "CREATE_POST_REQUEST";
+}
+interface CREATE_POST_SUCCESS {
+  type: "CREATE_POST_SUCCESS";
+  payload: IPost;
+}
+interface CREATE_POST_FAILURE {
+  type: "CREATE_POST_FAILURE";
+  error: any;
+}
+
+export type PostAction =
+  | CREATE_POST_REQUEST
+  | CREATE_POST_SUCCESS
+  | CREATE_POST_FAILURE;
