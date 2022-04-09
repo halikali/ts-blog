01b59c732ecb6af80fbef4a8306f4bd536c45be0
halikali <tunchalikali@gmail.com>
2022-04-09 23:45:39 +0300

    all post component is dynamic now

--- src/components/AllPosts/AllPosts.tsx
+++ src/components/AllPosts/AllPosts.tsx
@@ -1,36 +1,38 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+
+import { IAppState } from "types/generalTypes";
 import "./AllPosts.scss";
 
 const AllPosts = () => {
+  const posts = useSelector((state: IAppState) => state.AllPosts);
+
   return (
     <div className="all-posts mt-5">
-      {Array.from({ length: 5 }).map((item, i) => (
-        <Link to={"/details"}>
-          <div className="card" key={i}>
-            <div className="card__image-wrapper">
-              <img
-                src="https://image.shutterstock.com/image-photo/homemade-granola-nuts-raisins-kiwi-600w-636196073.jpg"
-                alt="post"
-                className="card__image"
-              />
+      {posts &&
+        posts.data.map((item) => (
+          <Link to={"/details"} key={item.id}>
+            <div className="card">
+              <div className="card__image-wrapper">
+                <img
+                  src={item.contentImageURL}
+                  alt="post"
+                  className="card__image"
+                />
+              </div>
+              <div className="card__body">
+                <p className="card__category-name">{item.category}</p>
+                <h4 className="card__title">{item.title}</h4>
+                <p className="card__author">
+                  {item.author}
+                  <span className="dot">•</span>
+                  <span className="card__date">20 april 2018</span>
+                </p>
+                <p className="card__content">{item.content}</p>
+              </div>
             </div>
-            <div className="card__body">
-              <p className="card__category-name">travel</p>
-              <h4 className="card__title">Lorem ipsum dolor sit amet.</h4>
-              <p className="card__author">
-                john Doe <span className="dot">•</span>
-                <span className="card__date">20 april 2018</span>
-              </p>
-              <p className="card__content">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, id
-                architecto sint quod facere omnis possimus vero fugiat
-                assumenda. Sequi nostrum error ipsam architecto itaque vitae
-                tenetur! Quae.
-              </p>
-            </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))}
 
       <p className="button button--red">load more</p>
     </div>
