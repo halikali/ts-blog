09e877a95765a0497342df21e311b50107aff3d1
halikali <tunchalikali@gmail.com>
2022-04-09 23:47:30 +0300

    detail page is dynamic now

--- src/pages/Detail-page/DetailPage.tsx
+++ src/pages/Detail-page/DetailPage.tsx
@@ -1,13 +1,21 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
 import Ads from "components/Ads";
 import CategoryList from "components/Category-List";
 import PopularPosts from "components/Popular-posts";
 import SocialMedia from "components/SocialMedia";
-import React, { useEffect, useState } from "react";
+import { getPostDetail, clearPostDetails } from "store/actions/postActions";
 import "./DetailPage.scss";
+import { IAppState } from "types/generalTypes";
 
 const DetailPage = () => {
-  const ımage = "https://preview.colorlib.com/theme/callie/img/media-2.jpg";
   const [scroll, setScroll] = useState(0);
+  const dispatch = useDispatch();
+  let { ID } = useParams();
+
+  const detail = useSelector((state: IAppState) => state.Detail?.data);
 
   useEffect(() => {
     window.addEventListener("scroll", (e: any) => {
@@ -20,31 +28,33 @@ const DetailPage = () => {
   }, [scroll]);
 
   useEffect(() => {
+    dispatch(getPostDetail(ID));
     window.scrollTo({
       top: 0,
       left: 0,
       behavior: "smooth",
     });
-  }, []);
+
+    return () => {
+      dispatch(clearPostDetails());
+    };
+  }, [ID]);
   return (
     <div className="row details">
       <div className="col-12 mb-5">
         <div
           className="details__header"
           style={{
-            backgroundImage: `url(${ımage})`,
+            backgroundImage: `url(${detail?.coverImageURL})`,
             backgroundPosition: `50% -${scroll}px`,
             transition: "all .3s ease",
           }}
         >
           <div className="details__text-wrapper">
-            <p className="details__category-name">lifestyle</p>
-            <h1 className="details__title">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore
-              itaque, sint ab eligendi neque optio
-            </h1>
+            <p className="details__category-name">{detail?.category}</p>
+            <h1 className="details__title">{detail?.title}</h1>
             <p className="details__author">
-              John doe <span className="dot"></span>
+              {detail?.author} <span className="dot"></span>
               <span className="details__created-date">20 april 2018</span>
             </p>
           </div>
@@ -52,82 +62,13 @@ const DetailPage = () => {
       </div>
       <div className="col-12 col-lg-8">
         <div className="details__body">
-          <h2>article title</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est omnis
-            fuga, similique nulla ullam quam tempore reprehenderit cumque unde
-            quas assumenda, libero soluta praesentium enim aliquam fugit optio
-            dolor veniam ab sapiente necessitatibus aliquid totam! Odio quasi,
-            cupiditate tempore officia, perspiciatis modi pariatur hic corporis
-            quibusdam unde veniam beatae optio neque minus consequatur ratione
-            impedit aperiam blanditiis? Veniam, quisquam eaque assumenda
-            mollitia quod nulla ut blanditiis sit ipsa expedita laborum et
-            dicta, error dignissimos ipsam temporibus vel debitis, illum
-            deleniti quas sapiente alias consequuntur doloribus? Expedita,
-            architecto repellendus exercitationem quibusdam sequi similique esse
-            perferendis provident optio aperiam vero adipisci ducimus?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est omnis
-            fuga, similique nulla ullam quam tempore reprehenderit cumque unde
-            quas assumenda, libero soluta praesentium enim aliquam fugit optio
-            dolor veniam ab sapiente necessitatibus aliquid totam! Odio quasi,
-            cupiditate tempore officia, perspiciatis modi pariatur hic corporis
-            quibusdam unde veniam beatae optio neque minus consequatur ratione
-            impedit aperiam blanditiis? Veniam, quisquam eaque assumenda
-            mollitia quod nulla ut blanditiis sit ipsa expedita laborum et
-            dicta, error dignissimos ipsam temporibus vel debitis, illum
-            deleniti quas sapiente alias consequuntur doloribus? Expedita,
-            architecto repellendus exercitationem quibusdam sequi similique esse
-            perferendis provident optio aperiam vero adipisci ducimus?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est omnis
-            fuga, similique nulla ullam quam tempore reprehenderit cumque unde
-            quas assumenda, libero soluta praesentium enim aliquam fugit optio
-            dolor veniam ab sapiente necessitatibus aliquid totam! Odio quasi,
-            cupiditate tempore officia, perspiciatis modi pariatur hic corporis
-            quibusdam unde veniam beatae optio neque minus consequatur ratione
-            impedit aperiam blanditiis? Veniam, quisquam eaque assumenda
-            mollitia quod nulla ut blanditiis sit ipsa expedita laborum et
-            dicta, error dignissimos ipsam temporibus vel debitis, illum
-            deleniti quas sapiente alias consequuntur doloribus? Expedita,
-            architecto repellendus exercitationem quibusdam sequi similique esse
-            perferendis provident optio aperiam vero adipisci ducimus?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est omnis
-            fuga, similique nulla ullam quam tempore reprehenderit cumque unde
-            quas assumenda, libero soluta praesentium enim aliquam fugit optio
-            dolor veniam ab sapiente necessitatibus aliquid totam! Odio quasi,
-            cupiditate tempore officia, perspiciatis modi pariatur hic corporis
-            quibusdam unde veniam beatae optio neque minus consequatur ratione
-            impedit aperiam blanditiis? Veniam, quisquam eaque assumenda
-            mollitia quod nulla ut blanditiis sit ipsa expedita laborum et
-            dicta, error dignissimos ipsam temporibus vel debitis, illum
-            deleniti quas sapiente alias consequuntur doloribus? Expedita,
-            architecto repellendus exercitationem quibusdam sequi similique esse
-            perferendis provident optio aperiam vero adipisci ducimus?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est omnis
-            fuga, similique nulla ullam quam tempore reprehenderit cumque unde
-            quas assumenda, libero soluta praesentium enim aliquam fugit optio
-            dolor veniam ab sapiente necessitatibus aliquid totam! Odio quasi,
-            cupiditate tempore officia, perspiciatis modi pariatur hic corporis
-            quibusdam unde veniam beatae optio neque minus consequatur ratione
-            impedit aperiam blanditiis? Veniam, quisquam eaque assumenda
-            mollitia quod nulla ut blanditiis sit ipsa expedita laborum et
-            dicta, error dignissimos ipsam temporibus vel debitis, illum
-            deleniti quas sapiente alias consequuntur doloribus? Expedita,
-            architecto repellendus exercitationem quibusdam sequi similique esse
-            perferendis provident optio aperiam vero adipisci ducimus?
-          </p>
+          <h2>{detail?.title}</h2>
+          <p>{detail?.content}</p>
 
           <img
             className="details__image"
-            src="https://preview.colorlib.com/theme/callie/img/media-2.jpg"
-            alt=""
+            src={detail?.contentImageURL}
+            alt={detail?.title}
           />
         </div>
       </div>
