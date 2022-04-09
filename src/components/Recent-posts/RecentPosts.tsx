3ceea5144b45f2c9bd93a0fe1e50a5d209d94d85
halikali <tunchalikali@gmail.com>
2022-04-09 23:46:41 +0300

    send props for recent posts component

--- src/components/Recent-posts/RecentPosts.tsx
+++ src/components/Recent-posts/RecentPosts.tsx
@@ -1,23 +1,27 @@
 import Post from "components/Post";
+import { useSelector } from "react-redux";
+import { IAppState } from "types/generalTypes";
 import "./RecentPosts.scss";
 
 const RecentPosts = () => {
+  const posts = useSelector((state: IAppState) => state.AllPosts);
+
   return (
     <div className="recent-posts">
       <h4 className="recent-posts__title">recent posts</h4>
 
       <div className="row">
         <div className="col-12 col-lg-6">
-          <Post type="type-1" />
+          <Post type="type-1" post={posts?.data[3]} />
         </div>
         <div className="col-12 col-lg-6">
-          <Post type="type-1" />
+          <Post type="type-1" post={posts?.data[4]} />
         </div>
         <div className="col-12 col-lg-6">
-          <Post type="type-1" />
+          <Post type="type-1" post={posts?.data[5]} />
         </div>
         <div className="col-12 col-lg-6">
-          <Post type="type-1" />
+          <Post type="type-1" post={posts?.data[6]}/>
         </div>
       </div>
     </div>
