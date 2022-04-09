780173ee8692e037bab4ac1a58465f32cd419867
halikali <tunchalikali@gmail.com>
2022-04-09 23:46:12 +0300

    send props for landing cards

--- src/components/Landing-cards/LandingCards.tsx
+++ src/components/Landing-cards/LandingCards.tsx
@@ -1,16 +1,20 @@
 import Post from "components/Post";
+import { useSelector } from "react-redux";
+import { IAppState } from "types/generalTypes";
 import "./LandingCards.scss";
 
 const LandingCards = () => {
+  const posts = useSelector((state: IAppState) => state.AllPosts);
+
   return (
     <div className="landing-cards">
       <div className="row">
         <div className="col-12 col-lg-8 first">
-          <Post />
+          <Post post={posts?.data[0]} />
         </div>
         <div className="col-12 col-lg-4 second">
-          <Post />
-          <Post />
+          <Post post={posts?.data[1]} />
+          <Post post={posts?.data[2]} />
         </div>
       </div>
     </div>
