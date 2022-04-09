707b8ddb158d817dfc1eee5a06ea967e6a8af9c3
halikali <tunchalikali@gmail.com>
2022-04-09 23:46:24 +0300

    post component is dynamic now

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,32 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { IPost } from "types/PostTypes";
 
 import "./Post.scss";
 
 interface IPostProps {
   type?: "type-1" | "type-2";
+  post?: IPost;
 }
 
 const Post: React.FC<IPostProps> = (props) => {
-  const { type } = props;
+  const { type, post } = props;
 
   return (
-    <Link to={"/details"}>
+    <Link to={`/details/${post?.id}`}>
       <div className={`post ${type || ""}`}>
         <div className="post__image-wrapper">
-          <img
-            src="https://image.shutterstock.com/image-photo/decorative-indoor-lightning-retro-bulbs-600w-2134575209.jpg"
-            alt="post"
-            className="post__image"
-          />
+          <img src={post?.contentImageURL} alt="post" className="post__image" />
         </div>
         <div className="post__info">
-          <p className="post__category">lifestyle</p>
-          <p className="post__text-content">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </p>
+          <p className="post__category">{post?.category}</p>
+          <p className="post__text-content">{post?.content}</p>
           <p className="post__author">
-            john Doe <span className="dot">•</span>
+            {post?.author} <span className="dot">•</span>
             <span className="post__date">20 april 2018</span>
           </p>
         </div>
